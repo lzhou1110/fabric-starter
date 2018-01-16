b555c09640da534095e5c2ddf7a2ad6ac812e860
Liyi Zhou <lzhou@Liyis-MacBook-Pro-2.local>
2018-01-16 16:56:59 +0400

    random stuff

--- chaincode/go/checker/chaincode_checker.go
+++ chaincode/go/checker/chaincode_checker.go
@@ -9,174 +9,68 @@ import (
 	"encoding/pem"
 	"crypto/x509"
 	"strings"
+	"encoding/json"
+	"time"
+	"fmt"
 )
 
 var logger = shim.NewLogger("CheckerChaincode")
+
+type Credit struct {
+	Borrower 	string		`json:"borrower"`
+	Amount 		int			`json:"amount"`
+}
 
 type CheckerChaincode struct {
 }
 
 func (t *CheckerChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	logger.Debug("Init")
-
-	_, args := stub.GetFunctionAndParameters()
-	var a, b string    // Entities
-	var aVal, bVal int // Asset holdings
-	var err error
-
-	if len(args) != 4 {
-		return pb.Response{Status:403, Message:"Incorrect number of arguments. Expecting 4"}
-	}
-
-	// Initialize the chaincode
-	a = args[0]
-	aVal, err = strconv.Atoi(args[1])
-	if err != nil {
-		return pb.Response{Status:403, Message:"Expecting integer value for asset holding"}
-	}
-	b = args[2]
-	bVal, err = strconv.Atoi(args[3])
-	if err != nil {
-		return pb.Response{Status:403, Message:"Expecting integer value for asset holding"}
-	}
-	logger.Debugf("aVal, bVal = %d", aVal, bVal)
-
-	// Write the state to the ledger
-	err = stub.PutState(a, []byte(strconv.Itoa(aVal)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	err = stub.PutState(b, []byte(strconv.Itoa(bVal)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
 	return shim.Success(nil)
 }
 
 func (t *CheckerChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+
 	logger.Debug("Invoke")
 
+	function, args := stub.GetFunctionAndParameters()
+	if function == "bankrupt" {
+		return t.bankrupt(stub, args)
+	} else if function == "tolerate" {
+		return t.tolerate(stub, args)
+	}
+	return pb.Response{Status:403, Message:"Invalid invoke function name."}
+}
+
+func (t *CheckerChaincode) bankrupt(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// Checking organization access right
 	creatorBytes, err := stub.GetCreator()
 	if err != nil {
 		return shim.Error(err.Error())
 	}
-
-	name, org := getCreator(creatorBytes)
-
-	logger.Debug("transaction creator " + name + "@" + org)
-
-	function, args := stub.GetFunctionAndParameters()
-	if function == "move" {
-		// Make payment of x units from a to b
-		return t.move(stub, args)
-	} else if function == "delete" {
-		// Deletes an entity from its state
-		return t.delete(stub, args)
-	} else if function == "query" {
-		// the old "Query" is now implemented in invoke
-		return t.query(stub, args)
+	_, org := getCreator(creatorBytes)
+	if org != "system" {
+		return pb.Response{Status:401,Message:"[%s] cannot call method", org}
 	}
 
-	return pb.Response{Status:403, Message:"Invalid invoke function name."}
-}
-
-// Transaction makes payment of x units from a to b
-func (t *CheckerChaincode) move(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var a, b string    // Entities
-	var aVal, bVal int // Asset holdings
-	var x int          // Transaction value
-	var err error
-
-	if len(args) != 3 {
-		return shim.Error("Incorrect number of arguments. Expecting 3")
-	}
-
-	a = args[0]
-	b = args[1]
-
-	// Get the state from the ledger
-	aBytes, err := stub.GetState(a)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-	if aBytes == nil {
-		return shim.Error("Entity not found")
-	}
-	aVal, _ = strconv.Atoi(string(aBytes))
-
-	bBytes, err := stub.GetState(b)
-	if err != nil {
-		return shim.Error("Failed to get state")
-	}
-	if bBytes == nil {
-		return shim.Error("Entity not found")
-	}
-	bVal, _ = strconv.Atoi(string(bBytes))
-
-	// Perform the execution
-	x, err = strconv.Atoi(args[2])
-	if err != nil {
-		return pb.Response{Status:403, Message:"Invalid transaction amount, expecting a integer value"}
-	}
-	aVal = aVal - x
-	bVal = bVal + x
-	logger.Debug("aVal = %d, bVal = %d\n", aVal, bVal)
-
-	// Write the state back to the ledger
-	err = stub.PutState(a, []byte(strconv.Itoa(aVal)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	err = stub.PutState(b, []byte(strconv.Itoa(bVal)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	return shim.Success(nil)
-}
-
-// deletes an entity from state
-func (t *CheckerChaincode) delete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// Reading input
 	if len(args) != 1 {
-		return pb.Response{Status:403, Message:"Incorrect number of arguments"}
+		return pb.Response{Status:403,Message:"Incorrect number of arguments"}
 	}
+	currentDate := args[0]
+	logger.Debugf("currentDate=%s", currentDate)
 
-	a := args[0]
+	functionArgs := make([][]byte, 2)
+	functionArgs[0] = []byte("due")
+	functionArgs[1] = []byte(currentDate)
 
-	// Delete the key from the state in ledger
-	err := stub.DelState(a)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
+	stub.InvokeChaincode("loan", functionArgs, "relatonship")
 
 	return shim.Success(nil)
 }
 
-// read value
-func (t *CheckerChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var a string // Entities
-	var err error
-
-	//if len(args) != 1 {
-	//	return pb.Response{Status:403, Message:"Incorrect number of arguments"}
-	//}
-
-	a = args[0]
-
-	// Get the state from the ledger
-	valBytes, err := stub.GetState(a)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	if valBytes == nil {
-		return shim.Error("Entity not found")
-	}
-
-	return shim.Success(valBytes)
+func (t *CheckerChaincode) tolerate(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
 }
 
 var getCreator = func (certificate []byte) (string, string) {
